4bce69d066c6697bb42f8e8b278503830351a16a
HarounTaHa <harountaha@outlook.sa>
2023-08-14 17:51:35 +0300

    refactor naming in setup module

--- _01_Flask_Web_Frameworks/flask-calculator/setup.py
+++ _01_Flask_Web_Frameworks/flask-calculator/setup.py
@@ -1,10 +1,10 @@
 from model import db, SavedTotal
 
 
-def run():
+def setup_database():
     db.connect()
     db.create_tables([SavedTotal])
 
 
 if __name__ == "__main__":
-    run()
+    setup_database()
