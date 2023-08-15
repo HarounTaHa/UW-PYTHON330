2258b7b2cf6fe7b9822001bd2ee3da0d9dec13e4
HarounTaHa <harountaha@outlook.sa>
2023-08-15 16:23:35 +0300

    create dotenv and update requirements

--- _01_Flask_Web_Frameworks/flask-calculator/main.py
+++ _01_Flask_Web_Frameworks/flask-calculator/main.py
@@ -1,12 +1,16 @@
 import base64
 import os
-
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, session
 from model import SavedTotal
 
 app = Flask(__name__)
 # os.urandom to generate key
-app.secret_key = b'\x16\x85\x9b\x14t\xc53\x0c\x03\xa3\xe6\xf7\xb2A\xa4\xdf\x91\xc8\x9c\x01Du\xc8>'
+# app.secret_key = b'\x16\x85\x9b\x14t\xc53\x0c\x03\xa3\xe6\xf7\xb2A\xa4\xdf\x91\xc8\x9c\x01Du\xc8>'
+
+# Load .env
+load_dotenv()
+app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 
 @app.route('/add', methods=['GET', 'POST'])
