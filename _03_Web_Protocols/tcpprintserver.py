686b7a002090f4408de9b110fd5230ed1cdbbe40
HarounTaHa <harountaha@outlook.sa>
2023-09-04 13:49:37 +0300

    snippet code for how define HTTP protocol TCP/IP

---
+++ _03_Web_Protocols/tcpprintserver.py
@@ -0,0 +1,33 @@
+import socket
+
+server = socket.socket()
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(('localhost', 8080))
+
+server.listen(1)
+
+n = 1
+
+while 1:
+    (client, address) = server.accept()
+    request = ""
+    while 1:
+        chunk = client.recv(1)
+
+        if chunk:
+            request += chunk.decode()
+
+        if request[-4:] == "\r\n\r\n":
+            break
+
+    response = "HTTP/1.1 200 OK\r\n"
+    response += "Content-TYpe: text\r\n"
+    response += "\r\n"
+    response += "You are the {}th visitor!".format(n)
+
+    client.sendall(response.encode())
+
+    client.close()
+    print(request)
+
+    n += 1
