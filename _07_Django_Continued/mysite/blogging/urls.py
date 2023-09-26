98fe33e1ca52243f39cb1516ca9e82d5c7629f66
HarounTaHa <harountaha@outlook.sa>
2023-09-26 15:59:11 +0300

    create detail_view and route for it

--- _07_Django_Continued/mysite/blogging/urls.py
+++ _07_Django_Continued/mysite/blogging/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from blogging.views import stub_view, list_view
+from blogging.views import  list_view, detail_view
 
 urlpatterns = [
     path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
+    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
 ]
