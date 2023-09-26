7d2909423cc0ef0967be114e7fcdfba4db73ee4b
HarounTaHa <harountaha@outlook.sa>
2023-09-26 15:44:10 +0300

    create list_view and route for it

---
+++ _07_Django_Continued/mysite/blogging/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from blogging.views import stub_view, list_view
+
+urlpatterns = [
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
+]
